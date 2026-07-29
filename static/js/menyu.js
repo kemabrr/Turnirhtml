b0e341d7c43b9dbb5ff35938a5e59611289bef6e
kemabrr <kemabr.uc@gmail.com>
2026-07-29 11:43:22 +0500

    Delete static/js directory

--- static/js/menyu.js
+++
@@ -1,6 +0,0 @@
-function logout() {
-            if (!confirm('Hakykatdanam cykmak isleyarsinizmi?')) return;
-            localStorage.removeItem('pubg_token');
-            localStorage.removeItem('pubg_admin_token');
-            window.location.href = './index.html';
-        }
